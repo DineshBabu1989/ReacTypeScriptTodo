59481e2ca0c046ae9da99a13fa3bd28387e11458
dinesh babu <Dinesh.babu@safetyio.com>
2021-05-06 12:34:16 +0530

    feat: add styling to the do app

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
-import { AddTodo } from '../types'
+import { AddTodo } from '../types';
+import styled from 'styled-components';
 
 interface Props {
   addTodo: AddTodo
 }
+
+const StyledButton = styled.button`
+  margin: 20px;
+  line-height: 50px;
+  min-width: 150px;
+  text-align: center;
+  background-color: #FD310F;
+  border-radius: 5px;
+  color: #fff;
+  border: 0;
+  cursor: pointer;
+  box-shadow: 0 0 5px rgb(0 0 0 / 50%);
+  transition: background-color 0.3s ease-in-out;
+  font-size: 18px;
+
+  &:disabled {
+    background-color: #e4ada4
+  }
+`
+const StyledInput = styled.input`
+  color: #333;
+  width: 300px;
+  box-sizing: border-box;
+  letter-spacing: 1px;
+  border: 0;
+  padding: 7px 0;
+  border-bottom: 1px solid #ccc;
+
+  &:focus-visible{
+    outline: none;
+    border-bottom: 1px solid #FD310F;
+  }
+`;
+
+const StyledInputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const TodoInput: React.FC<Props> = ({addTodo}) => {
   const [text, setText] = React.useState<string>('');
@@ -19,10 +60,10 @@ const TodoInput: React.FC<Props> = ({addTodo}) => {
   }
 
   return (
-    <div>
-      <input type="text" value={text} onChange={handleOnChange}/> 
-      <button onClick={handleSubmit} disabled={!text}>Add todo</button>
-    </div>
+    <StyledInputWrapper>
+      <StyledInput type="text" value={text} onChange={handleOnChange}/> 
+      <StyledButton onClick={handleSubmit} disabled={!text}>Add todo</StyledButton>
+    </StyledInputWrapper>
   )
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,28 +1,87 @@
-import React from 'react';
-import { Todo } from '../types';
+import React from "react";
+import { Todo } from "../types";
+import styled from "styled-components";
 
 interface Props {
-  todos: Todo[],
-  deleteTodo: (index: number) => void,
-  completeTodo: (index: number) => void
+  todos: Todo[];
+  deleteTodo: (index: number) => void;
+  completeTodo: (index: number) => void;
 }
 
-const TodoList: React.FC<Props> = ({todos, deleteTodo, completeTodo}) => {
-  const listItems = Array.isArray(todos) ? 
+const StyledList = styled.li`
+  list-style: none;
+  letter-spacing: 1px;
+  border: 1px solid #f77777;
+  border-radius: 5px;
+  padding: 10px;
+  width: 500px;
+  background-color: white;
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledCompleteButton = styled.button<{ completed: boolean }>`
+  line-height: 30px;
+  min-width: 150px;
+  text-align: center;
+  border: 1px solid #f77777;
+  border-radius: 5px;
+  color: ${(props) => (props.completed ? "white" : "black")};
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  font-size: 14px;
+  background-color: ${(props) => (props.completed ? "#FD310F" : "white")};
+`;
+const StyledDeleteButton = styled.button`
+  line-height: 30px;
+  min-width: 50px;
+  text-align: center;
+  border: 1px solid #f77777;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  font-size: 24px;
+  background-color: white;
+`;
+
+const StyledText = styled.button`
+  line-height: 30px;
+  letter-spacing: 1px;
+  width: 300px;
+  background-color: white;
+  border: none;
+`;
+
+const StyledListContainer = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TodoList: React.FC<Props> = ({ todos, deleteTodo, completeTodo }) => {
+  const listItems = Array.isArray(todos) ? (
     todos.map((todo, i) => (
-    <li key={i} >
-      {todo.text}
-      <button onClick={() => completeTodo(i)}>
-        { todo.completed ? 'Complete' : 'Incomplete'}
-      </button>
-      <button onClick={() => deleteTodo(i)}>x</button>
-    </li>)) :
-    <li>Loading...</li>;
-  return (
-    <ul>
-      {listItems}
-    </ul>
-  )
-}
+      <StyledList key={i}>
+        <StyledText>{todo.text}</StyledText>
+        <StyledCompleteButton
+          onClick={() => completeTodo(i)}
+          completed={todo.completed}
+        >
+          {todo.completed ? "Complete" : "In complete"}
+        </StyledCompleteButton>
+        <StyledDeleteButton onClick={() => deleteTodo(i)}>x</StyledDeleteButton>
+      </StyledList>
+    ))
+  ) : (
+    <li>Loading...</li>
+  );
+  return <StyledListContainer>{listItems}</StyledListContainer>;
+};
 
 export default TodoList;
